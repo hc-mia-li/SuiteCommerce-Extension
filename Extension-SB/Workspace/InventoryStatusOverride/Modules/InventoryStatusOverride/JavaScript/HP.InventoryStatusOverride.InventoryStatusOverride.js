abff9498b2504dc8fddc0e6e18aff65ca0a53dd8
hc-mia-li <mia.li@funacc.cn>
2025-10-23 13:31:50 +0800

    PDP也显示缺货信息

--- Extension-SB/Workspace/InventoryStatusOverride/Modules/InventoryStatusOverride/JavaScript/HP.InventoryStatusOverride.InventoryStatusOverride.js
+++ Extension-SB/Workspace/InventoryStatusOverride/Modules/InventoryStatusOverride/JavaScript/HP.InventoryStatusOverride.InventoryStatusOverride.js
@@ -15,32 +15,29 @@ define(
 	return {
 		mountToApp: function mountToApp(container) {
 			var env = container.getComponent("Environment");
-			var plp = container.getComponent("PLP");
 
-			if(plp)
-			{
-				let items = env.getConfig("InventoryStatus.Items")||[];
-				_.extend(ProductLineStockView.prototype, {
-					getContext: _.wrap(ProductLineStockView.prototype.getContext, function (fn) {
-						var context = fn.apply(this, arguments);
-						var self = this;
-						var matchedItem = _.find(items, function(item) {
-							return item.itemId === self.model.get('itemid');
-						});
+			let items = env.getConfig("InventoryStatus.Items")||[];
+			_.extend(ProductLineStockView.prototype, {
+				getContext: _.wrap(ProductLineStockView.prototype.getContext, function (fn) {
+					var context = fn.apply(this, arguments);
+					var self = this;
+					var matchedItem = _.find(items, function(item) {
+						let itemid = self.model.get('itemid')?self.model.get('itemid'):self.model.get('item').get('itemid')
+						return item.itemId === itemid;
+					});
 
-						if (matchedItem) {
-							context.showOutOfStockMessage = true;
-							if (matchedItem.OutOfStockMessage) {
-								context.stockInfo.outOfStockMessage = matchedItem.OutOfStockMessage;
-							}
-							context.stockInfo.stock = 0;
-							context.stockInfo.isInStock = false;
-							context.stockInfo.showOutOfStockMessage = true;
+					if (matchedItem) {
+						context.showOutOfStockMessage = true;
+						if (matchedItem.OutOfStockMessage) {
+							context.stockInfo.outOfStockMessage = matchedItem.OutOfStockMessage;
 						}
-						return context;
-					})
-				});
-			}
+						context.stockInfo.stock = 0;
+						context.stockInfo.isInStock = false;
+						context.stockInfo.showOutOfStockMessage = true;
+					}
+					return context;
+				})
+			});
 		}
 	}
 });
